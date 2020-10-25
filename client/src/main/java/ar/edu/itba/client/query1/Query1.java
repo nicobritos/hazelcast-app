cfc3f5571ca35f872eef0f0adac70fc93ef45ccc
Agustín Roca <aroca@itba.edu.ar>
2020-10-25 14:36:15 -0300

    Sort quey1 results

--- client/src/main/java/ar/edu/itba/client/query1/Query1.java
+++ client/src/main/java/ar/edu/itba/client/query1/Query1.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.client.query1;
 
 import ar.edu.itba.api.Tree;
+import ar.edu.itba.api.query1.Query1Result;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
@@ -11,17 +12,19 @@ import com.hazelcast.mapreduce.Job;
 import com.hazelcast.mapreduce.JobTracker;
 import com.hazelcast.mapreduce.KeyValueSource;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class Query1 {
-    private ICompletableFuture<Map<String, Double>> result;
+    private final ICompletableFuture<Map<String, Double>> result;
 
-    public Query1() throws ExecutionException, InterruptedException {
+    public Query1() {
         final ClientConfig ccfg = new ClientConfig();
         final HazelcastInstance hz = HazelcastClient.newHazelcastClient(ccfg);
         JobTracker jobTracker = hz.getJobTracker("trees-per-person");
-        final IMap<String, Long> populations = hz.getMap("populations");
+        final IMap<String, Long> populations = hz.getMap("populations-map");
         final IList<Tree> trees = hz.getList("tree-list");
         final KeyValueSource<String, Tree> source = KeyValueSource.fromList(trees);
 
@@ -33,8 +36,12 @@ public class Query1 {
                 .submit(new TreesPerPersonCollator(populations));
     }
 
-    public Map<String, Double> getResult() throws ExecutionException, InterruptedException {
+    public List<Query1Result> getResult() throws ExecutionException, InterruptedException {
         // Wait and retrieve the result
-        return result.get();
+        Map<String, Double> ans = result.get();
+
+        List<Query1Result> list = Query1Result.listFrom(ans);
+        Collections.sort(list);
+        return list;
     }
 }

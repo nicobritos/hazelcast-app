970dd79e63cbf9100887810d35a4fc9a9c505d5e
gbarbieripederiva <gbarbieripederiva@gmail.com>
2020-10-25 01:43:43 -0300

    init archetype

---
+++ server/src/main/java/ar/edu/itba/server/Server.java
@@ -0,0 +1,12 @@
+package ar.edu.itba.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server {
+    private static Logger logger = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        logger.info("tp2hazelcast Server Starting ...");
+    }
+}

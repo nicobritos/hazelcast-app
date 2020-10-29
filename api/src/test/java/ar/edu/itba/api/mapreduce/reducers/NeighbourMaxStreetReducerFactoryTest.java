74b3a4d332e98b23c163ef7c518859809b16f48b
Ignacio <igrasso@itba.edu.ar>
2020-10-29 20:29:00 -0300

    all reducers tested

---
+++ api/src/test/java/ar/edu/itba/api/mapreduce/reducers/NeighbourMaxStreetReducerFactoryTest.java
@@ -0,0 +1,82 @@
+package ar.edu.itba.api.mapreduce.reducers;
+
+import ar.edu.itba.api.City;
+import ar.edu.itba.api.Tree;
+import ar.edu.itba.api.mapreduce.combiners.StreetCountCombinerFactory;
+import ar.edu.itba.api.mapreduce.mappers.StreetCountMapper;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IList;
+import com.hazelcast.map.impl.MapEntrySimple;
+import com.hazelcast.mapreduce.Job;
+import com.hazelcast.mapreduce.JobCompletableFuture;
+import com.hazelcast.mapreduce.JobTracker;
+import com.hazelcast.mapreduce.KeyValueSource;
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class NeighbourMaxStreetReducerFactoryTest {
+    private Collection<Tree> treesCollection = new ArrayList<Tree>();
+    private HazelcastInstance hazelcast;
+    private HazelcastInstance hazelcastClient;
+    private JobTracker jobTracker;
+    private IList<Tree> iTreeList;
+
+
+    @Before
+    public void initialize() {
+        Config serverConfig = new Config();
+
+        hazelcast = Hazelcast.newHazelcastInstance(serverConfig);
+        hazelcastClient = HazelcastClient.newHazelcastClient();
+        jobTracker = hazelcastClient.getJobTracker("trees-per-person");
+        iTreeList = hazelcastClient.getList("tree-list");
+        treesCollection.add(new Tree(City.CABA,"sp1","n1","st1",13.0));
+        treesCollection.add(new Tree(City.CABA,"sp2","n2","st1",17.0));
+        treesCollection.add(new Tree(City.CABA,"sp3","n1","st1",11.0));
+        treesCollection.add(new Tree(City.CABA,"sp2","n3","st1",27.0));
+        treesCollection.add(new Tree(City.CABA,"sp1","n1","st1",22.0));
+        treesCollection.add(new Tree(City.CABA,"sp1","n4","st1",24.0));
+        treesCollection.add(new Tree(City.CABA,"sp3","n1","st1",15.0));
+        treesCollection.add(new Tree(City.CABA,"sp3","n5","st1",21.0));
+        treesCollection.add(new Tree(City.CABA,"sp2","n3","st1",19.0));
+        treesCollection.add(new Tree(City.CABA,"sp1","n2","st1",29.0));
+    }
+
+    @After
+    public void shutdown() {
+        hazelcast.shutdown();
+        hazelcastClient.shutdown();
+    }
+
+    @Test
+    public void getTotalQtyOfTreesByStreetOfEachNeighbourhood() throws ExecutionException, InterruptedException {
+        iTreeList.addAll(this.treesCollection);
+        final KeyValueSource<String, Tree> source = KeyValueSource.fromList(iTreeList);
+        final Job<String, Tree> job = jobTracker.newJob(source);
+        JobCompletableFuture<Map<String, MapEntrySimple<String, Long>>> result = job.mapper(new StreetCountMapper())
+                .combiner(new StreetCountCombinerFactory())
+                .reducer(new NeighbourMaxStreetReducerFactory()).submit();
+        Map<String, MapEntrySimple<String, Long>> ans = result.get();
+        Assert.assertEquals(ans.get("n1").getKey(),"st1");
+        Assert.assertEquals(ans.get("n1").getValue().intValue(),4);
+        Assert.assertEquals(ans.get("n2").getKey(),"st1");
+        Assert.assertEquals(ans.get("n2").getValue().intValue(),2);
+        Assert.assertEquals(ans.get("n3").getKey(),"st1");
+        Assert.assertEquals(ans.get("n3").getValue().intValue(),2);
+        Assert.assertEquals(ans.get("n4").getKey(),"st1");
+        Assert.assertEquals(ans.get("n4").getValue().intValue(),1);
+        Assert.assertEquals(ans.get("n5").getKey(),"st1");
+        Assert.assertEquals(ans.get("n5").getValue().intValue(),1);
+
+    }
+}
